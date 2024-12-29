dab7625c360da334421d55f83ef58a49aaa79ced
ankush.kumar <ankush.kumar@infoaxon.com>
2024-12-29 19:56:44 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/MainNav";
-import Movies from "./Pages/Movies/Movies";
+import Movies from "./Pages/Movies";
 import Series from "./Pages/Series/Series";
 import Trending from "./Pages/Trending/Trending";
 import Search from "./Pages/Search/Search";
